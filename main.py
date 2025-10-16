75190985fa00f647bf04d68030259612c1d34412
Himas1321 <nikita5045@mail.ru>
2025-10-16 22:52:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from urllib.parse import urlparse
 import os
 from dotenv import load_dotenv
 import requests
 load_dotenv()
 
 
-def shorten_link(api_key, api_url, user_url):
-	
+def shorten_link(api_key, user_url):
+	api_url = "https://api.vk.ru/method/utils.getShortLink"
 	params = {
 		"url": user_url,
 		"access_token": api_key,
@@ -14,25 +15,58 @@ def shorten_link(api_key, api_url, user_url):
 	
 	response = requests.get(api_url, params=params)
 	response.raise_for_status()
-	short_url = response.json()
-	short = short_url["response"]["short_url"]
-	return short
+	data = response.json()
+
+	if "error" in data:
+		error_msg = data["error"]["error_msg"]
+		raise requests.exceptions.HTTPError(error_msg)
+	
+
+	return data["response"]["short_url"]
+
+
+def count_clicks(api_key, short_url):
+	api_url = "https://dev.vk.ru/ru/method/utils.getLinkStats"
+	key = urlparse(short_url).path.lstrip('/')
+	params = {
+		"key":key,
+		"access_token": api_key,
+		"interval": "forever",
+		"extended": "0",
+		"v": "5.199",
+	}
+	
+	response = requests.get(api_url, params=params)
+	response.raise_for_status()
+
+	data = response.json()
+
+	if "error" in data:
+		error_msg = data["error"]["error_msg"]
+		raise requests.exceptions.HTTPError(error_msg)
+	print(data)
+
+	return data["response"]["views"]
 
 
 def main():
 
 	user_url = input('Введите ссылку ')
 	api_key = os.getenv('API_KEY')
-	api_url = 'https://api.vk.ru/method/utils.getShortLink'
-	print('Сокращенная ссылка:', shorten_link(api_key, api_url, user_url))
+
+	
 	
 	try:
-		short = shorten_link(api_key, api_url, user_url)
-	except :
-		print('ошибка')
+		short_link = shorten_link(api_key, user_url)
+		print('Сокращенная ссылка:', short_link)
+
+		clicks = count_clicks(api_key, short_link)
+		print("Количество переходов:", clicks)
+
+	except requests.exceptions.HTTPError as e:
+		print('ошибка', e)
 
 
 
 if __name__ == '__main__':
 	main()
-	
